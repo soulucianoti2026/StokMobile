8e40fe2f1a0e4f60637063285decc3838b1c9d87
Luciano Azevedo Almeida <soulucianoti@gmail.com>
2026-09-24 19:09:48 -0300

    Revert "feature: develop login screen and authentication logic"

--- lib/homepage/home_controller.dart
+++
@@ -1,3 +0,0 @@
-part of 'home_controller.dart';
-
-class HomepageController {}
--- lib/loginpage/model/user.dart
+++
@@ -1,6 +0,0 @@
-class User {
-  final String email;
-  final String senha;
-
-  User({required this.email, required this.senha});
-}
